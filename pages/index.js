ca3d09ba1ea625e7097606d6ba64d0772ff06fec
Jacob David Diehl <jakediehl17@gmail.com>
2024-06-18 07:49:48 -0600

    fixed weird spaccing

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Diehl Domain</title>
         <meta name="description" content="Welcome to my personal site!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
