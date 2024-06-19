4a8ef49233bd4680fd840ef8bb7febb03c7cc053
Jacob David Diehl <jakediehl17@gmail.com>
2024-06-18 23:06:49 -0600

    removed dynamic design

--- components/homePageComponents/Results/index.js
+++ components/homePageComponents/Results/index.js
@@ -8,13 +8,13 @@ export default function Results(){
                     <p className="featureBox__text">Require an email list? I utilize Nodemailer for automated emails. Need real-time messaging in your application? I can integrate Socket.io, SWR, or PeerJS for video communication using WebRTC</p>
                 </div>
             </div>
-            <div className="col-1-of-4">
+            {/* <div className="col-1-of-4">
                 <div className="featureBox">
                     <i className="featureBox__icon icon-basic-smartphone "></i>
                     <h3 className="heading-tertiary">Dynamic Design</h3>
                     <p className="featureBox__text">Crafted for ultimate customization, styling packages include predefined variables for spacing and color, ensuring seamless resizing from desktop to mobile and effortless comparison of color choices.</p>
                 </div>
-            </div>
+            </div> */}
             <div className="col-1-of-4">
                 <div className="featureBox">
                     <i className="featureBox__icon icon-basic-cloud "></i>
