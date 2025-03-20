45b183c3f80d5ec69a9722463d047b5933e48bb0
Damien Clark <damienclark@Damiens-MacBook-Pro.local>
2025-03-20 07:52:30 -0600

    updated next.config.mjs to ignore test files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.externals = [...(config.externals || []), ({ request }, callback) => {
+        if (request && request.match(/\.test\.(js|ts|jsx|tsx)$/)) {
+          return callback(null, 'commonjs ' + request);
+        }
+        callback();
+      }];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
