1ca448efd11ee26f1f4c9352a1b20845c6d4247e
Damien Clark <damienclark@Damiens-MacBook-Pro.local>
2025-03-20 09:14:11 -0600

    move tests out of pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.module.rules.push({
-        test: /\.test\.(js|ts|jsx|tsx)$/,
-        use: 'null-loader', // Prevents Next.js from processing test files
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
